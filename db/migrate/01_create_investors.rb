05bd64477b17c3a7652f3f6cc61f885bfe1f133c
Carlton Grandison <carltondgrandison@gmail.com>
2019-06-25 17:05:18 -0600

    application

--- db/migrate/01_create_investors.rb
+++ db/migrate/01_create_investors.rb
@@ -1,10 +1,12 @@
-class CreateInvestorTable < ActiveRecord::Migration[5.1]
+class CreateInvestorTable < ActiveRecord::Migration
 
     def change 
         create_table :investor do |t|
             t.string :name
             t.string :email_address
             t.string :password_digest
+
+            t.timestamps null:false
         end 
     end
 end 
--- db/migrate/02_create_investments.rb
+++ db/migrate/02_create_investments.rb
@@ -1,4 +1,4 @@
-class CreateInvestmentTable < ActiveRecord::Migration[5.1]
+class CreateInvestmentTable < ActiveRecord::Migration
 
     def change 
         create_table :investments do |t|
@@ -6,6 +6,8 @@ class CreateInvestmentTable < ActiveRecord::Migration[5.1]
             t.integer :amount_invested
             t.integer :years_until_return
             t.integeer :investor_id
+
+            t.timestamps null:false
         end 
     end
 end 
